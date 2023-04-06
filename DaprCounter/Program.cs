d2c686941fa5187eb0784a2b9d7ad0f0c505f0f0
Tony Nguyen <tony.nguyen@ansarada.com>
2023-04-06 18:44:56 +0700

    Dapr state management

--- DaprCounter/Program.cs
+++ DaprCounter/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dapr.Client;
+
+const string storeName = "statestore";
+const string key = "counter";
+
+Console.WriteLine($"TEST NE CON");
+
+var daprClient  = new DaprClientBuilder().Build();
+var counter = await  daprClient.GetStateAsync<int>(storeName, key);
+
+while (true)
+{
+    Console.WriteLine($"Counter = {counter++}");
+
+    await daprClient.SaveStateAsync(storeName, key, counter);
+    await Task.Delay(1000);
+}
